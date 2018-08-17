9f35f64c9b4c57faed7f4e6d93681bdafa4549cb
Wei-Ting Lin <weiting.dev@gmail.com>
2018-08-16 18:11:58 -0700

    Transitioning from Bootstrap to Materialize
    Changing styling using Materialize and SASS due to its simplistic, flat, modern look and feel.

--- js/defer.js
+++ js/defer.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+    $('body').scrollspy({target: '.navbar', offset: 50});
+    $('.sidenav').sidenav();
+
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) { //when user scrolls down
             $('#topBtn').fadeIn();
@@ -9,19 +12,10 @@ $(document).ready(function() {
     });
 
     $('#topBtn').click(function() {
-        $("html, body").animate({
+        $('html, body').animate({
             scrollTop: 0 //when btn is clicked, scrolls to top
         }, 300); //speed of animation
         return false;
-    });
-
-    // Clicking on Portfolio leads to section
-    var heightFromTop = $("#portfolio").offset().top - $('header > nav').outerHeight();
-
-    $(".portfolio").click(function() {
-        $('html,body').animate({
-            scrollTop: heightFromTop},
-            'slow');
     });
 
 });
