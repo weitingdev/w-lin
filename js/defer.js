eefc49cd2ad27affdf3a42ad600797a1e15203b7
Wei-Ting Lin <weiting.dev@gmail.com>
2018-08-20 18:31:23 -0700

    Edited COZY project page and landing page hero text.
    Changed card sizes for wireframes and prototypes for better legibility. Made Screenflow image responsive. Added hero header text and call-to-action button. Changed nav bar to reflect page content for COZY project page. Changed main page hero for legibility (temporary).

--- js/defer.js
+++ js/defer.js
@@ -29,4 +29,25 @@ $(document).ready(function() {
     var elems = document.querySelectorAll('.scrollspy');
     var instances = M.ScrollSpy.init(elems);
 
+    //Animation by TobiasAhlin.com
+    $('.ml16').each(function(){
+        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+    });
+    anime.timeline({loop: true})
+    .add({
+        targets: '.ml16 .letter',
+        translateY: [-100,0],
+        easing: "easeOutExpo",
+        duration: 2400,
+        delay: function(el, i) {
+          return 30 * i;
+        }
+    }).add({
+        targets: '.ml16',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
 });
